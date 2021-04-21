14889bfebc1e55c8e6832c966ab05e53ab021287
Mateo <mateo.avila313@gmail.com>
2021-04-20 23:49:30 -0700

    outline

--- Sources/easyFire/easyFire.swift
+++ Sources/easyFire/easyFire.swift
@@ -1,11 +1,27 @@
+
 public struct easyFire {
-    var text = "Hello, World!"
-    
+    var url =  "https://moneysaver-b9ab0-default-rtdb.firebaseio.com/"
     public init(){
+    }
+    
+    public mutating func setPath(path: String){
+        url = url + path
+    }
+   
+    public func delete(url: String){
         
     }
-    
-    public func add() -> String{
-        return "it was added"
+    public func add(from url: String){
+        
     }
+    public func update(url: String){
+        
+    }
+}
+
+
+private struct myResults:Codable{
+    let item: String
+
+    
 }
